ced94278445ac31866e36a7c85b1a03445b67d5c
paul <pnheinsohn@uc.cl>
2019-10-28 20:29:47 +0100

    Done part3_1 & partially done part3_2 (missing Alex output)

--- segunda practica/part3_1.py
+++ segunda practica/part3_1.py
@@ -26,13 +26,13 @@ class AESCipher:
 
 
 if __name__ == "__main__":
-    # bin_key = open_file(path="files/2019_09_25_17_02_54_alex.valls", ext="key")
-    bin_key = open_file(ext="key")
+    # bin_key = open_file(ext="key")
+    bin_key = open_file(path="files/2019_09_25_17_02_54_alex.valls", ext="key")
     aes_crypt = AESCipher(bin_key)
 
-    # bin_msg = open_file(path="files/2019_09_25_17_02_54_alex.valls")
-    bin_msg = open_file()
+    # bin_msg = open_file()
+    bin_msg = open_file(path="files/2019_09_25_17_02_54_alex.valls")
     decrypted = aes_crypt.decrypt(bin_msg)
     
-    save_file("decrypted_paul.jpeg", decrypted)
-    # save_file("decrypted_alex.jpeg", decrypted)
+    # save_file("decrypted_paul.jpeg", decrypted)
+    save_file("decrypted_alex.jpeg", decrypted)
--- segunda practica/part3_2.py
+++ segunda practica/part3_2.py
@@ -5,7 +5,18 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
 
-ASCII = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+ASCII = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#$%&/()=¿?¡!*~^{ }[]-_:;.,<>+\"'|°¬´¨"
+
+def gen_key():
+    for a in ASCII:
+        for b in ASCII:
+            yield f"{a * 8}{b * 8}"
+
+def gen_id():
+    id_ = 0
+    while True:
+        yield id_
+        id_ += 1
 
 def open_file(path="files/2019_09_25_17_01_52_paul.heinsohn.puerta_trasera.enc"):
     with open(f"{path}", 'rb') as f:
@@ -16,38 +27,7 @@ def save_file(path, decrypted):
         f.write(decrypted)
     print(f"Succesful completion: {path} has been created")
 
-class Keygen():
-
-    def __init__(self):
-        self.H = hashlib.sha256(self._pre_master_key()).digest()
-        self.key = self.H[:16]
-        self.iv =  self.H[16:]
-
-    def _single_key(self):
-        ascii_chars = ""
-        while len(ascii_chars) != 16:
-            char = choice(ASCII)
-            if char not in ascii_chars:
-                ascii_chars += f"{char}" * 8
-        return ascii_chars
-
-    def _pre_master_key(self):
-        res = self._single_key()
-        for _ in range(7): res = ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(res, self._single_key()))
-        return res.encode()
-
-    def print_key(self):
-        print(f'''
-        * Key *
-            - Key: {self.key}
-            - Len: {len(self.key)}
-        --------------------------------------------------------------
-        * iv *
-            - iv: {self.iv}
-            - Len: {len(self.iv)}
-        ''')
-
-class AESCipher():
+class AESCipher:
 
     def __init__(self, key, iv):
         self.key = key
@@ -57,12 +37,24 @@ class AESCipher():
         cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
         return unpad(cipher.decrypt(enc), AES.block_size)
 
-
 if __name__ == "__main__":
-    keygen = Keygen()
-    aes = AESCipher(keygen.key, keygen.iv)
-    
-    data = open_file()
-    decrypted = aes.decrypt(data)
+    # data = open_file()
+    data = open_file("files/2019_09_25_17_02_54_alex.valls.puerta_trasera.enc")
+    key_generator = gen_key()
+    id_generator = gen_id()
 
-    save_file("decrypted.txt", decrypted)
+    while True:
+        try:
+            H = hashlib.sha256(next(key_generator).encode()).digest()
+            key = H[:16]
+            iv = H[16:]
+            aes = AESCipher(key, iv)
+            decrypted = aes.decrypt(data)
+        except ValueError:
+            pass
+        except StopIteration:
+            break
+        else:
+            id__ = next(id_generator)
+            # save_file(f"new/decrypted_paul.puerta_trasera_{id__}", decrypted)
+            save_file(f"new/decrypted_alex.puerta_trasera_{id__}", decrypted)
